9702ed98060073992f64ae8091968748ee12ac19
kukuhap06 <kukuhap407@gmail.com>
2025-04-17 09:04:55 +0700

    file1

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+        <table border="1">
+            <thead>
+                <tr>
+                    <td>JUDUL</td>
+                    <td>KATEGORI</td>
+                    <td>DURASI</td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>AOT</td>
+                    <td>ANIME</td>
+                    <td>144 menit</td>
+                </tr>
+            </tbody>
+        </table>
+</body>
+</html>
